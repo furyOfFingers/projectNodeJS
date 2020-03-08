b4f8a322aba8fde40be8505e1a396b902a63dd9d
Marat <maratformusic@gmail.com>
2020-03-08 13:48:18 +0300

    очередная попытка доставть данные из тела запроса

--- index.js
+++ index.js
@@ -1,75 +1,114 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const { parse } = require('querystring');
+const bodyParser = require('body-parser');
+
+
 
 const server = http.createServer((req, res) => {
-  // if (req.url === '/') {
-  //   fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-  //     if (err) {
-  //       throw err
-  //     }
-  //
-  //     res.writeHead(200, {
-  //       'Content-Type': 'text/html'
-  //     })
-  //     res.end(data)
-  //   })
-  // } else if (req.url === '/contact') {
-  //   fs.readFile(path.join(__dirname, 'public', 'contact.html'), (err, data) => {
-  //     if (err) {
-  //       throw err
-  //     }
-  //
-  //     res.writeHead(200, {
-  //       'Content-Type': 'text/html'
-  //     })
-  //     res.end(data)
-  //   })
-  // }
+  let argFilePath;
 
-  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
-  const ext = path.extname(filePath)
-  let contentType = 'text/html'
+  switch (req.url) {
+    case "/?log=admin&pas=admin":
+      argFilePath = "home.html";
+      break;
+
+    case "/":
+      argFilePath = "index.html";
+      break;
+
+    default:
+      argFilePath = req.url;
+  }
+  console.log(req.url, "req.url");
+  let filePath = path.join(__dirname, "public", argFilePath);
+  const ext = path.extname(filePath);
+  let contentType = "text/html";
 
   switch (ext) {
-    case '.css':
-      contentType = 'text/css'
-      break
-    case '.js':
-      contentType = 'text/javascript'
-      break
+    case ".css":
+      contentType = "text/css";
+      break;
+
+    case ".js":
+      contentType = "text/javascript";
+      break;
+
     default:
-      contentType = 'text/html'
+      contentType = "text/html";
   }
 
   if (!ext) {
-    filePath += '.html'
+    filePath += ".html";
   }
 
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+      fs.readFile(path.join(__dirname, "public", "error.html"), (err, data) => {
         if (err) {
-          res.writeHead(500)
-          res.end('Error')
+          res.writeHead(500);
+          res.end("Error");
         } else {
           res.writeHead(200, {
-            'Content-Type': 'text/html'
-          })
-          res.end(data)
+            "Content-Type": "text/html"
+          });
+          res.end(data);
         }
-      })
+      });
     } else {
       res.writeHead(200, {
-        'Content-Type': contentType
-      })
-      res.end(content)
+        "Content-Type": contentType
+      });
+      res.end(content);
     }
-  })
-})
+  });
 
-const PORT = process.env.PORT || 3000
+  let body;
+  //прием, который надо вынести
+  if (req.method === "POST") {
+    console.log(req.method, "method");
+    req.on("data", function(data) {
+      body = data;
+    });
 
-server.listen(PORT, () => {
-  console.log(`Server has been started on ${PORT}...`)
-})
+    req.on("end", function() {
+      // console.log('parseBody', parse(body));
+      console.log('body["log"], body["pas"]', body['log'], body['pas']);
+      console.log('body.log, body.pas', body.log, body.pas);
+      console.log("typeof Body: " + typeof body);
+      console.log("Body: " + body);
+      console.log("log and pas", body.log, body.pas);
+      console.log('stringornot', body[0], body[1])
+      // response.writeHead(200, { "Content-Type": "text/html" });
+    });
+    for(let el in body) {
+      console.log('let el in body', el)
+    }
+    // for(let el of body) {
+    //   console.log('let el of body', el)
+    // }
+    if (body && body.log == "admin" && body && body.pas == "admin") {
+      console.log("WOWWOWWOW");
+    }
+  }
+
+  // server.use(bodyParser.json());
+  // server.use(bodyParser.urlencoded({ extended: true }));
+
+  // server.get('/user', function (req, res, next) {
+  //   let data = req.body;
+
+  //   data.forEach(function (el) {
+  //     console.log(el.log);
+  //     console.log(el.pas);
+  //   });
+  // });
+});
+
+const HOSTNAME = "127.0.0.1";
+const PORT = process.env.PORT || 8888;
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server has been started on http://${HOSTNAME}:${PORT}/`);
+});
---
+++ public/js/xhr.js
@@ -0,0 +1,36 @@
+const requestURL = "catchThis";
+//Необходимо открывать именно по этому адресу, а не localhost
+const formRequest = "http://127.0.0.1:8888";
+let sendBtn = document.querySelector("#sendBtn");
+
+loginFunc = () => {
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", formRequest);
+  let inputs = document.querySelectorAll('input[type="text"]');
+
+  let userData = {
+    log: inputs[0].value,
+    pas: inputs[1].value
+  };
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send(JSON.stringify(userData));
+  xhr.onload = function() {
+    console.log(this.responseURL, "responseURL");
+    console.log(this.responseType, "responseType");
+    console.log(this.HEADERS_RECEIVED, "HEADERS_RECEIVED");
+  };
+  xhr.onerror = function() {
+    alert("server error");
+  };
+};
+
+sendBtn && sendBtn.addEventListener("click", loginFunc);
+
+//START canceled change page
+let formBlock = document.querySelector("form");
+formPrevent = event => {
+  event.preventDefault();
+};
+formBlock.addEventListener("click", formPrevent);
+// END
